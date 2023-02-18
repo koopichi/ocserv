b0e4dc20c34e52e1e9d37e203691cb1070429096
koopichi <82188509+koopichi@users.noreply.github.com>
2023-02-18 21:02:44 +0300

    Update dk.sh

--- dk.sh
+++ dk.sh
@@ -22,4 +22,4 @@ echo \
 sudo apt-get update
 
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
